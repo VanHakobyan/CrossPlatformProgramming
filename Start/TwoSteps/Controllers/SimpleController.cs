fe53a643493d9f0c64097fcd2537d151ba2a2822
Vanik Hakobyan <vanhakobyan1996@gmail.com>
2017-09-29 18:11:35 +0400

    add first steps code :ok:

---
+++ Start/TwoSteps/Controllers/SimpleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwoSteps.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Simple")]
+    public class SimpleController : Controller
+    {
+        // GET: api/Simple
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Simple/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Simple
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Simple/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
