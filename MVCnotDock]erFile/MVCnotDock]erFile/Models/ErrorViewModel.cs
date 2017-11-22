ea49e96c89f14aa6e18bf287f1a3bfc51b883a55
van hakobyan <van19962013@mail.ru>
2017-11-22 16:32:47 +0400

    add simple MVCs

---
+++ MVCnotDock]erFile/MVCnotDock]erFile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCnotDock_erFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
